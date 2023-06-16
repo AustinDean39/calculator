d39385a3400eb258e088a0f2dd72babc3ea06440
Austin Dean <19595957+AustinDean39@users.noreply.github.com>
2023-06-16 10:06:56 -0400

    Add functionality for non-numbers

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 // Basic calculation functions:
 // Add two numbers
 function add(num1, num2) {
-  return num1 + num2;
+  return parseFloat(num1) + parseFloat(num2);
 }
 
 // Subtract two numbers
 function subtract(num1, num2) {
-  return num1 - num2;
+  return parseFloat(num1) - parseFloat(num2);
 }
 
 // Multiply two numbers
 function multiply(num1, num2) {
-  return num1 * num2;
+  return parseFloat(num1) * parseFloat(num2);
 }
 
 // Divide two numbers
 function divide(num1, num2) {
-  return num1 / num2;
+  return parseFloat(num1) / parseFloat(num2);
 }
