eb5062f28dbd3a64c5fb0243cca472fb15d17aa2
Austin Dean <19595957+AustinDean39@users.noreply.github.com>
2023-06-16 10:06:56 -0400

    Create subtraction function

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-// basic calculation functions:
+// Basic calculation functions:
 // Add two numbers
 function add(num1, num2) {
   return num1 + num2;
 }
+
+// Subtract two numbers
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
